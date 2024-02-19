0a7aea3dc0cf6d8a4a242639cb4adc40a2f34e42
lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-20 00:11:11 +0200

    fix BaseModle __init__ method to add new obj

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 import models
+from models import storage
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
@@ -31,6 +32,8 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+
+        storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
