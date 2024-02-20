c10c7db1bd01e92722f8b17d1c4e2a33a887245b
lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-20 22:52:40 +0200

    Edit city, place, user class

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -12,3 +13,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship("User", cascade='all, delete, delete-orphan',
+                          backref="user")
