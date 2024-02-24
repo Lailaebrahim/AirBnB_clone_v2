1e43ccac4c235e96d84f9f07b3a62300292fe553
lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-24 23:52:35 +0200

    Fix Importing

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.place import Place, place_amenity
+from models.place import place_amenity
 from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
 
