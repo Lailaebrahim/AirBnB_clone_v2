dd1b8754aa40acb9fa730966ffcd3f5d760d8876
Lailaebrahim <ebrahimlaila815@gmail.com>
2024-03-09 13:43:48 -0800

    Task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#Bash script that sets up your web servers for the deployment of web_static.
+sudo apt-get -y update
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo echo "<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome</title>
+    </head>
+    <body>
+        <h1>Welcome to my page!</h1>
+        <p>I hope you find what you're looking for.</p>
+    </body>
+</html> " >> /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sed -i '53i\\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+sudo service nginx restart
