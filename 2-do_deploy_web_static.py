ca0ca4592cbead049dc43803edf6ee17cfdf311d
lailaebrahim <ebrahimlaila815@gmail.com>
2024-03-10 15:04:00 +0200

    Fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,9 +3,10 @@
 import os
 from fabric.api import *
 
-env.hosts = [ '54.174.80.164', '54.161.236.106' ]
-env.user = 'ubuntu'
 
+env.host = ['54.174.80.164', '54.161.236.106']
+env.user = 'ubuntu'
+env.priv_key = '~/etc/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
@@ -15,19 +16,17 @@ def do_deploy(archive_path):
     # Return False if archive doesn't exist
     if not os.path.exists(archive_path):
         return False
-
-    # Creating the name of the archive using the current date and time
-    now = datetime.now()
-    archive_name = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
-
-    # Creating the command to generate the archive
-    command = "tar -czvf versions/{} web_static".format(archive_name)
-
-    # Executing the command using Fabric's local function
-    result = local(command)
-
-    # Checking if the archive has been correctly generated
-    if result.failed:
-        return None
-    else:
-        return "versions/{}".format(archive_name)
+    try:
+        archive = archive_path.split('/')[-1]
+        put(archive_path, '/tmp/{}'.format(archive))
+        run('mkdir -p /data/web_static/releases/{}'
+            .format(archive.split('.')[0]))
+        run('tar -xvzf /tmp/{} -C /data/web_static/releases/{}'
+            .format(archive, archive.split('.')[0]))
+        run('rm -f /tmp/{}'.format(archive))
+        run("rm -rf /data/web_static/current")
+        run("ln -sf /data/web_static/releases/{} /data/web_static/current"
+            .archive.split('.')[0])
+        return True
+    except:
+        return False
