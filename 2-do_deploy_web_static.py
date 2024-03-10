901887ff2ddcff7c058dade68e2e69def9693d2b
lailaebrahim <ebrahimlaila815@gmail.com>
2024-03-10 16:09:02 +0200

    test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,12 +20,12 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/{}'.format(archive))
         run('mkdir -p /data/web_static/releases/{}/'
             .format(archive.split('.')[0]))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(archive, archive.split('.')[0]))
-        run('rm -f /tmp/{}'.format(archive))
-        run("rm -rf /data/web_static/current")
-        run("ln -sf /data/web_static/releases/{} /data/web_static/current"
-            .archive.split('.')[0])
+        #run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+        #    .format(archive, archive.split('.')[0]))
+        #run('rm -f /tmp/{}'.format(archive))
+        #run("rm -rf /data/web_static/current")
+        #run("ln -sf /data/web_static/releases/{} /data/web_static/current"
+        #    .archive.split('.')[0])
         return True
     except:
         return False
