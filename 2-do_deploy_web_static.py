604e788c0a33047d7e7bd758c31afac6489340b0
Lailaebrahim <lailaebrahim815@gmail.com>
2024-03-10 14:46:35 -0700

    Task 2 V2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,18 +14,21 @@ def do_deploy(archive_path):
         return False
     try:
         archive = archive_path.split('/')[-1]
-        arc = archive.split('.')[0]
-        put(archive_path, "/tmp/")
-        run('mkdir -p /data/web_static/releases/{}/'.format(arc))
+        archive_no_ext = archive.split('.')[0]
+        data_path = '/data/web_static/releases'
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}/'
+            .format(archive_no_ext))
         run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(archive, arc))
+            .format(archive, archive_no_ext))
         run('rm -f /tmp/{}'.format(archive))
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/'
-            .format(arc, arc))
-        run('rm -rf /data/web_static/releases/{}/web_static/'.format(arc))
+        run('mv {}/{}/web_static/* /data/web_static/releases/{}'
+            .format(data_path, archive_no_ext, archive_no_ext))
+        run('rm -rf /data/web_static/releases/{}/web_static/'
+            .format(archive_no_ext))
         run("rm -rf /data/web_static/current")
-        run("ln -sf /data/web_static/releases/{}/  /data/web_static/current".
-            format(arc))
+        run("ln -sf /data/web_static/releases/{}/  /data/web_static/current"
+            .format(archive_no_ext))
         return True
     except:
         return False
