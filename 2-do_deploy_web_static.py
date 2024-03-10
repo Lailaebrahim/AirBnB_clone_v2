7c2266556c200a9a7c881169ed4dab2103a6fff8
lailaebrahim <ebrahimlaila815@gmail.com>
2024-03-10 22:00:30 +0200

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,20 +12,23 @@ def do_deploy(archive_path):
     """
     Method Deploy Web statis Files to web servers
     """
-
     # Return False if archive doesn't exist
     if os.path.isfile(archive_path) is False:
         return False
     try:
         archive = archive_path.split('/')[-1]
+        arc = archive.split('.')[0]
         put("{}".format(archive_path), "/tmp/{}".format(archive))
-        run('mkdir -p /data/web_static/releases/{}/'.format(archive.split('.')[0]))
-        #run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-        #    .format(archive, archive.split('.')[0]))
-        #run('rm -f /tmp/{}'.format(archive))
-        #run("rm -rf /data/web_static/current")
-        #run("ln -sf /data/web_static/releases/{} /data/web_static/current"
-        #    .archive.split('.')[0])
+        run('mkdir -p /data/web_static/releases/{}/'.format(arc))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(archive, arc))
+        run('rm -f /tmp/{}'.format(archive))
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/'
+            .format(arc, arc))
+        run('rm -rf /data/web_static/releases/{}/web_static/'.format(arc))
+        run("rm -rf /data/web_static/current")
+        run("ln -sf /data/web_static/releases/{}/  /data/web_static/current".
+            format(arc))
         return True
     except:
         return False
