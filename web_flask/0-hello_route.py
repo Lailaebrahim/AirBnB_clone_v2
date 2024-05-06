1c85f35bedeaf532cf7dc866e199ee9d3cbd5a66
Lailaebrahim <lailaebrahim815@gmail.com>
2024-05-06 12:49:46 -0700

    Fix PEP8 Errors

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,16 +4,15 @@ Module define the rout URL
 """
 
 
-
 from flask import Flask
 app = Flask(__name__)
-
 
 
 @app.route('/', strict_slashes=False)
 def index():
     """root Method"""
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
