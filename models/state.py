e77477f5487d72cd4a15ac8d0db2440da5a77f6e
Lailaebrahim <lailaebrahim815@gmail.com>
2024-06-10 21:45:47 +0000

    Add method to return list of cities

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
 import models
 from models.city import City
 from models.base_model import BaseModel, Base
@@ -12,15 +13,17 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", cascade='all, delete, delete-orphan',
+                              backref="state")
+    else:
 
-    @property
-    def cities(self):
-        from models import storage
-        list_obj = []
-        objs = storage.all('City')
-        for key, val in objs.items():
-            if val.state_id == self.id:
-                list_obj.append(val)
-        return list_obj
+        @property
+        def cities(self):
+            from models import storage
+            list_obj = []
+            objs = storage.all('City')
+            for key, val in objs.items():
+                if val.state_id == self.id:
+                    list_obj.append(val)
+            return list_obj
